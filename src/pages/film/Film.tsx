dd88073517a8f7a10ab8a7ce412fc3247f727ebc
Alex-nk89 <nagaytsev_ae@vk.com>
2022-10-12 10:24:05 +0300

    feature: create query getFilm

--- src/pages/film/Film.tsx
+++ src/pages/film/Film.tsx
@@ -1,3 +1,22 @@
+import { useMemo } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+import { Skeleton, Typography } from '@mui/material';
+import { FilmServices } from '../../services/services';
+
+import style from './film.module.scss';
+import { PageTitle } from '../../component/common/page-title/PageTitle';
+
 export const Film: React.FC = () => {
-    return <h3>Film</h3>;
+    const { kinopoiskId } = useParams();
+    const { data, isLoading, isError } = useQuery(['film'], () =>
+        FilmServices.getFilm(Number(kinopoiskId))
+    );
+
+    const title = useMemo(() => {
+        const title = `${data?.data.nameRu} (${data?.data.year})`;
+        return <PageTitle isLoading={isLoading} title={title} />;
+    }, [data?.data.nameRu, data?.data.year, isLoading]);
+
+    return <div className={style.film_wrapper}>{title}</div>;
 };
---
+++ src/pages/film/film.module.scss.d.ts
@@ -0,0 +1,1 @@
+export const film_wrapper: string;
