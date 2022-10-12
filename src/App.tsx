7d523776024db16c25dfb317e62f67b39e24f916
Alex-nk89 <nagaytsev_ae@vk.com>
2022-10-12 14:35:19 +0300

    main: test cicd

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ function App() {
                     <Route path='/' element={<Home />} />
                     <Route path='/film/:kinopoiskId' element={<Film />} />
                 </Routes>
-                <div>test</div>
             </Container>
         </>
     );
