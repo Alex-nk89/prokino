a6e9e3419d314047cacd02faeb7f7620375b0f25
NagaytsevAE <nagaytsev_ae@vk.com>
2022-10-26 10:31:21 +0300

    test

--- src/component/common/header-page/HeaderPage.tsx
+++ src/component/common/header-page/HeaderPage.tsx
@@ -1,36 +1,31 @@
-import { Details, IDetail } from '../details/Details';
-import { Poster } from '../poster/Poster';
-import style from './headerPage.module.scss';
-import { Skeleton } from '@mui/material';
-import { FC } from 'react';
+import { Details, IDetail } from "../details/Details";
+import { Poster } from "../poster/Poster";
+import style from "./headerPage.module.scss";
+import { Skeleton } from "@mui/material";
+import { FC } from "react";
 
 interface IHeaderPage {
-    detailsList: IDetail[];
-    posterUrl: string;
-    titleForDetail?: string;
-    isLoading?: boolean;
+  detailsList: IDetail[];
+  posterUrl: string;
+  titleForDetail?: string;
+  isLoading?: boolean;
 }
 
 export const HeaderPage: FC<IHeaderPage> = ({
-    posterUrl,
-    detailsList,
-    titleForDetail,
-    isLoading,
+  posterUrl,
+  detailsList,
+  titleForDetail,
+  isLoading,
 }) => {
-    if (isLoading) {
-        return <Skeleton height={400} variant='rounded' />;
-    }
-    return (
-        <section className={style.cover}>
-            <div className={style.cover_details}>
-                <Poster
-                    src={posterUrl}
-                    alt='Обложка'
-                    width={230}
-                    height={330}
-                />
-                <Details details={detailsList} title={titleForDetail} />
-            </div>
-        </section>
-    );
+  if (isLoading) {
+    return <Skeleton height={400} variant="rounded" />;
+  }
+  return (
+    <section className={style.cover}>
+      <div className={style.cover_details}>
+        <Poster src={posterUrl} alt="Обложка" width={230} height={330} />
+        <Details details={detailsList} title={titleForDetail} />
+      </div>
+    </section>
+  );
 };
